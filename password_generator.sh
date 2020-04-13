03183c490bbc6419dd14d4c5d80eb9eddfadcdaf
Catherine <yacatherine19@gmail.com>
2020-04-13 23:49:41 +0800

    added input options

--- password_generator.sh
+++ password_generator.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
-read -p 'Type the length of the password: ' LENGTH
+## Instead of handling user inputs via `read`, this script handles user inputs via input options
+#read -p 'Type the length of the password: ' LENGTH
+
+## Print manual while any error occurs
+usage() {
+    echo "Usage: ${0} [-vs] [-l LENGTH]" >&2
+    echo 'Generate a random password'
+    echo '  -l LENGTH  Specify a password length'
+    echo '  -s         Append a special character to the password'
+    echo '  -v         Increase verbosity'
+    exit 1
+}
+
+## Set a default password length
+LENGTH=48
+
+## Input options.
+while getopts vl:s OPTION
+do
+    case ${OPTION} in
+        v)
+            VERBOSE='true'
+            echo 'Verbose mode on'
+            ;;
+        l)
+            LENGTH="${OPTARG}"
+            ;;
+        s)
+            USE_SPECIAL_CHARACTERS='true'
+            ;;
+        ?)
+            Usage
+            ;;
+    esac
+done
+
 
 ## Define special characters
 S="!@#$%^&*()_-+="
@@ -11,5 +46,5 @@ SPECIAL_CHAR=$(echo "${S}" | fold -b1 | shuf | head -n1)
 PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c${LENGTH-1})
 echo "${PASSWORD}${SPECIAL_CHAR}"
 
-# To notify user that this script is done successfully.
+## Notify user that this script is done successfully.
 exit 0
